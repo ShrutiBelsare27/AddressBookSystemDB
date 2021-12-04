68fc49caf5565adea3f6be1498edf9c5ec751ec8
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2021-12-04 19:16:15 +0530

    Added: Insert new contact in address book

---
+++ insert_data.sql
@@ -0,0 +1,7 @@
+insert into Address_Book values('shruti','belsare','shivaji nagar','amravati','maharashtra','444601','8149713160','shruti@gmail.com');
+insert into Address_Book values('suraj','pawar','mataji negar','pune','karnataka','415673','8149613160','suraj@gmail.com');
+insert into Address_Book values('komal','joshi','bashweshwar','amravati','andhra','413678','8179713160','komal123@gmail.com');
+insert into Address_Book values('trimbak','pawar','shivaji chowk','mumbai','up','748949','8149703160','trimbak@gmail.com');
+select * from Address_Book;
+
+
